36d55065c8116ea4096502b99258910b794549c7
Anjali Singh <muskan.anjali.singh@gmail.com>
2020-06-21 03:22:06 +0530

    Dashboard contents added

--- dashboard-applicant.php
+++ dashboard-applicant.php
@@ -20,6 +20,7 @@ if(!isset($_SESSION['userid']))
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins&family=Roboto+Mono:wght@700&family=Roboto+Slab&family=Source+Code+Pro:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/dashboard.css">
+
 </head>
 <body>	
 	<div class="sidebar">
@@ -35,6 +36,46 @@ if(!isset($_SESSION['userid']))
 			<li><i class="fa fa-user-circle-o icon"></i><a href="#" id="username" style="font-family: 'Roboto Mono', monospace;">Hi! <?php echo $_SESSION["userid"]; ?></a></li>
 		</ul>
 	</nav>
+</div>
+<div class="content container-fluid">
+	<center><h1>INTERVIEW <b>GUIDER</b></h1></center><br>
+	<center><p>Hi, we will be guiding you through your selection & interview process.</p></center><br>
+	<div class="row">
+		<div class="col-sm-2">
+			
+		</div>
+		<div class="card col-sm-6">
+			<h2>
+				<center>Step 1: TAKING THE PSYCHOMETRIC TEST</center>
+			</h2>
+			<center><p>To make the recruitement process more transparent and fair, we have included an Intelligent ChatBot to help you out.</p>
+			<p>For moving on further to the interview stage, you have to qualify the test.</p>
+			<p>The "CHATBOT" option is present on the Menu.</p><p> Click on that to proceed.</p><br></center>
+		</div>
+		
+	</div>
+	<br><br>
+	<div class="row">
+		<div class=" card col-sm-5">
+			<h2>
+				<center>Step 2: RESULT GENERATION</center>
+			</h2>
+			<center><p>The results will be provided as soon as you "SUBMIT" the test.</p>
+			<p>Note: You can only take the test once for every job profile. If you FAIL, you can not proceed to the interview stage.</p><br></center>
+		</div>
+		<div class="card col-sm-5">
+			<h2>
+				<center>Step 3: SCHEDULING INTERVIEW</center>
+			</h2>
+			<center><p>Once you pass the test, your details would be provided to the recruiter. </p>
+			<p>The recruiter would then provide an interview date, which would be immediately notified via your chatbot.</p>
+			<br>
+			</center>
+		</div>
+	</div>
+	<center><p>
+		<br><br>So what are you waiting for? Hop right into it.
+	</p></center><br>
 </div>
 </body>
 </html>
