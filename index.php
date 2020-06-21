7cb9df3d2044141a2cebee9d2e389a690ecc3075
Anjali Singh <muskan.anjali.singh@gmail.com>
2020-06-21 11:27:19 +0530

    Index Page Initial

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AIBot</title>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins&family=Roboto+Mono:wght@700&family=Roboto+Slab&family=Source+Code+Pro:wght@500&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/index_style.css">
+
+</head>
+<body>
+	<div class="navbar container-fluid">
+			<a href="login-recruiter.php" role="button" class="btn btn-lg">
+				LOGIN AS RECRUITER
+			</a>
+			<a href="login.php" role="button" class="btn btn-lg">
+				LOGIN AS APPLICANT
+			</a>
+
+			<a href="index.php" id="logo">AIBot</a>
+		</div>
+	<div class="container-fluid">
+		<div class="row row-1">
+			<center><h1 class="first">Manage your job applications with</h1><h1> full tranparency using</h1>
+				<h1 >AIBot!!</h1>
+				<BR>
+				<p>A web-app to decrease the workload from <br>the recruiters, and to make the recruitement process more diverse.</p><br><br><br>
+				<a href="registration.php" class="btn btn-color btn-lg" role="button">
+					JOIN AS APPLICANT
+				</a>
+				<a href="registration-recruiter.php" class="btn btn-color btn-lg" role="button">
+					JOIN AS RECRUITER
+				</a>
+			</center>
+		</div>
+	</div>
+	<div class="container-fluid tab2">
+		<div class="row row-3">
+			<center><br><br>
+				<h2>How Does This Work?</h2><br>
+				<p>The main idea of this is to ease the workload of the recruiters, by only giving them the shortlisted applicants. It also makes the recruitement process a lot easier for the applicants. Anyone can apply for any job, but before that, they have to qualify a certain test. <br>
+					Baaki add kadio mereko samajh nahi aaraha hai.<br><br><br>
+				</p>
+			</center>
+		</div>
+	</div>
+	<div class="container-fluid tab3">
+		<div class="row row-2">
+			<center><br><br>
+				<h2> Why BillDesk?</h2>
+			</center>
+			<br><br>
+			<div class="row">
+				<div class="col-sm-6 cols">
+					<center>
+						<i class="fa fa-bell-o icons"></i>
+						<h3> NOTIFICATIONS</h3>
+						<p>Reminds a week before renewal date of inventory,<br> and two months before the expiry of goods.</p>
+					</center>				
+				</div>
+				<div class="col-sm-6 cols">
+					<center>
+						<i class="fa fa-credit-card-alt icons"></i>
+						<h3>CREATING BILLS</h3>
+						<p>Helps in creation of SMS bills for cafes/restaurants,<br> and has a customizable invoice generator for startups or freelancers.</p>
+					</center>				
+				</div>
+			</div>
+			<br><br><br>
+			<div class="row">
+				<div class="col-sm-6 cols">
+					<center>
+						<i class="fa fa-cutlery icons"></i>
+						<h3>ADD ITEMS</h3>
+						<p>Add or remove items from your inventory.<br> 
+						It helps you keep a check on the raw materials.</p>
+					</center>				
+				</div>
+				<div class="col-sm-6 cols">
+					<center>
+						<i class="fa fa-money icons"></i>
+						<h3>MANAGE SELLINGS</h3>
+						<p>You can supervise your sellings and savings by the end <br>of each month.All the bills generated are kept at one place.</p>
+					</center>				
+				</div>
+			</div>
+			<br><br>
+		</div>
+	</div>
+</body>
+</html>
